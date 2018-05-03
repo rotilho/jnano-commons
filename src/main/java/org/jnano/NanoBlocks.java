64587753c928b8ea006db11407fdac8d9c6f92ca
Felipe Rotilho <felipe@rotilho.com>
2018-05-03 23:04:55 +0200

    Add main net genesis block

--- src/main/java/org/jnano/NanoBlocks.java
+++ src/main/java/org/jnano/NanoBlocks.java
@@ -7,6 +7,8 @@ import static org.jnano.DataUtils.radix;
 import static org.jnano.DataUtils.toByteArray;
 
 public final class NanoBlocks {
+    public final static String MAIN_NET_GENESIS = "991CF190094C00F0B68E2E5F75F6BEE95A2E0BD93CEAA4A6734DB9F19B728948";
+
     private final static byte[] STATE_BLOCK_PREAMBLE = toByteArray("0000000000000000000000000000000000000000000000000000000000000006");
 
     private NanoBlocks() {

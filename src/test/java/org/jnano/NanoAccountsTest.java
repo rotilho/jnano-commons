e85d7eed1b900e9cb8052a1f11967547cb7634cc
Felipe Rotilho <felipe@rotilho.com>
2018-05-09 19:22:33 +0200

    Fix NanoAccount test

--- src/test/java/org/jnano/NanoAccountsTest.java
+++ src/test/java/org/jnano/NanoAccountsTest.java
@@ -8,14 +8,22 @@ import java.util.List;
 import static org.junit.Assert.*;
 
 public class NanoAccountsTest {
-    private static final byte[] PRIVATE_KEY = NanoKeys.createPrivateKey("1234567890123456789012345678901234567890123456789012345678901234", 0);
+    private static final String SEED = "1234567890123456789012345678901234567890123456789012345678901234";
     private static final String ACCOUNT = "nano_3iwi45me3cgo9aza9wx5f7rder37hw11xtc1ek8psqxw5oxb8cujjad6qp9y";
     private static final String OLD_ACCOUNT = "xrb_3iwi45me3cgo9aza9wx5f7rder37hw11xtc1ek8psqxw5oxb8cujjad6qp9y";
     private static final List<String> ACCOUNTS = Arrays.asList(ACCOUNT, OLD_ACCOUNT);
 
     @Test
     public void shouldCreateAccount() {
-        assertEquals(ACCOUNT, NanoAccounts.createAccount(PRIVATE_KEY));
+        // given
+        byte[] privateKey = NanoKeys.createPrivateKey(SEED, 0);
+        byte[] publicKey = NanoKeys.createPublicKey(privateKey);
+
+        // when
+        String account = NanoAccounts.createAccount(publicKey);
+
+        // then
+        assertEquals(ACCOUNT, account);
     }
 
     @Test(expected = IllegalArgumentException.class)
